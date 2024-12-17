44660ad98f18e056a588dc20574a5b3d3539126c
mathysfruchard <mathys.fruchard@stfelixlasalle.fr>
2024-12-17 11:52:32 +0100

    maj

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, request, jsonify, redirect, s
 import os
 
 app = Flask(__name__)
-app.secret_key = 'secret_key'  # Clé secrète pour stocker des sessions
+app.secret_key = 'secret_key'  # Clé secrète pour stocker les sessions
 
 # Configuration du dossier static pour les images
 IMAGE_FOLDER = 'static'
@@ -24,15 +24,35 @@ def add_to_cart():
     if "cart" not in session:
         session["cart"] = []
 
-    session["cart"].append({"image": image, "quantity": quantity})
-    session.modified = True  # Pour sauvegarder les modifications
+    # Ajouter au panier ou mettre à jour la quantité si déjà existant
+    for item in session["cart"]:
+        if item["image"] == image:
+            item["quantity"] += quantity
+            break
+    else:
+        session["cart"].append({"image": image, "quantity": quantity})
 
+    session.modified = True
     return jsonify({"status": "success", "image": image, "quantity": quantity})
 
 @app.route("/cart")
 def cart():
     cart_items = session.get("cart", [])
     return render_template("cart.html", cart_items=cart_items, title="Récapitulatif de commande")
+
+@app.route("/update_cart", methods=["POST"])
+def update_cart():
+    data = request.json
+    action = data.get("action")
+    image = data.get("image")
+
+    if "cart" in session:
+        session["cart"] = [
+            {"image": item["image"], "quantity": item["quantity"] - 1} if action == "decrease" and item["image"] == image and item["quantity"] > 1 else
+            item for item in session["cart"] if not (action == "delete" and item["image"] == image)
+        ]
+    session.modified = True
+    return jsonify({"status": "success", "cart": session["cart"]})
 
 if __name__ == "__main__":
     app.run(debug=True)
